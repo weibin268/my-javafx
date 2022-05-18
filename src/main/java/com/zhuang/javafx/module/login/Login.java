6b60049d3bd96f51ec77a1cdfc2b1c7a1b5a85f9
zwb <binweizhuang@hotmail.com>
2022-05-18 22:10:48 +0800

    aa

--- src/main/java/com/zhuang/javafx/module/login/Login.java
+++ src/main/java/com/zhuang/javafx/module/login/Login.java
@@ -1,10 +1,20 @@
 package com.zhuang.javafx.module.login;
 
 import com.zhuang.javafx.common.Window;
+import com.zhuang.javafx.utils.AlertUtils;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
 public class Login extends Window {
 
+    public TextField username;
+    public TextField password;
+
     public Login() {
         super("Login", 300, 200);
+    }
+
+    public void onLogin(ActionEvent actionEvent) {
+        AlertUtils.info(username.getText() + "-" + password.getText());
     }
 }
