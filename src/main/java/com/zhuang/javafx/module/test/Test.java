6198c55b5b3a8a6b5bf97a4830dcd3f4aaea2d94
庄伟斌 <zhuangweibin@qq.com>
2022-05-26 18:30:12 +0800

    aa

---
+++ src/main/java/com/zhuang/javafx/module/test/Test.java
@@ -0,0 +1,11 @@
+package com.zhuang.javafx.module.test;
+
+import com.zhuang.javafx.common.Window;
+import com.zhuang.javafx.utils.AlertUtils;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Test extends Window {
+
+
+}
